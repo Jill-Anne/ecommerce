c24ca0b47901f4e271b82180c569bd04d52f5a99
Jill <jillannebragado13@gmail.com>
2024-06-09 17:26:02 +0800

    create a footer, provider to access all product in app.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {ShopCategory} from './Pages/ShopCategory';
 import {Product} from './Pages/Product'
 import {LoginSignup} from './Pages/LoginSignup'
 import {Cart} from './Pages/Cart'
+import Footer from './Components/Footer/Footer';
 
 
 function App() {
@@ -25,8 +26,8 @@ function App() {
         <Route path= '/cart' element={<Cart/>}/>
         <Route path= '/login' element={<LoginSignup/>}/>
 
-
       </Routes>
+      <Footer/>
       </BrowserRouter>
     </div>
   );
